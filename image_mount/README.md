e807944eb5531086030efa2b0017d95722d43bc1
Thammi <thammi@chaossource.net>
2014-11-09 17:55:15 +0100

    Add image mount

---
+++ image_mount/README.md
@@ -0,0 +1,17 @@
+# image mount
+
+This script is designed to help you mount disk images. It will parse the
+partition table of the image using `sfdisk` and print the mount command for each
+partition.
+
+Example usage:
+
+    $ ./image_mount.py test.img
+    test.img1
+    size:   3000.0 MB
+    mount:  mount -o loop,offset=1048576 /tmp/test.img /mnt
+
+    test.img2
+    size:   11258.0 MB
+    mount:  mount -o loop,offset=3146776576 /tmp/test.img /mnt
+
---
+++ image_mount/image_mount.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import os.path
+import subprocess
+
+def find_sfdisk():
+    locations = ['/sbin/sfdisk']
+
+    for location in locations:
+        if os.path.exists(location):
+            return location
+
+    return None
+
+def parse_line(line):
+    start = line.find(':')
+
+    name = line[:start].strip()
+
+    obj = {}
+
+    for part in line[start+1:].split(','):
+        parts = [i.strip() for i in part.split('=', 1)]
+
+        if len(parts) == 2:
+            key, raw = parts
+
+            if re.match('\d+', raw):
+                value = int(raw)
+            else:
+                value = raw
+
+            obj[key] = value
+        else:
+            key = parts[0]
+            obj[key] = True
+
+    return name, obj
+
+def to_bytes(value):
+    return value * 512
+
+def get_partitions(path):
+    sfdisk = find_sfdisk()
+
+    # run fsdisk
+
+    cmd = [sfdisk, '-ld', path]
+    raw = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    out = raw.decode('utf-8').strip()
+
+    # looks like actual output?
+
+    if not re.search('^# partition table of ', out):
+        raise Exception("sfdisk seems to be unable to read the image")
+
+    # find start of table
+
+    start = out.index('\n\n')
+
+    if start == -1:
+        raise Exception("Unable to parse output, cannot find start of table")
+
+    # parse table
+
+    lines = out[start:].split('\n')[2:]
+
+    partitions = []
+
+    for line in lines:
+        name, data = parse_line(line)
+
+        if data['start'] == 0:
+            continue
+
+        partitions.append((name, data))
+
+    return partitions
+
+def print_partitions(path, mount_point):
+    partitions = get_partitions(path)
+
+    abs_path = os.path.abspath(path)
+
+    for name, data in partitions:
+        mount = 'mount -o loop,offset={} {} {}'.format(to_bytes(data['start']), abs_path, mount_point)
+
+        print(name)
+        print('size:\t{} MB'.format(to_bytes(data['size']) / 1024 / 1024))
+        print('mount:\t{}'.format(mount))
+        print()
+
+def main(args):
+    if len(args) < 2:
+        print("Usage: {} IMAGE [MOUNT_POINT]".format(args[0]))
+        return 1
+
+    path = args[1]
+    mount_point = args[2] if len(args) > 2 else '/mnt'
+
+    try:
+        print_partitions(path, mount_point)
+    except Exception as e:
+        print(e)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
